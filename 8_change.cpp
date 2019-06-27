30a5af79c53508808f35c97027988df47f96ffbe
Liu Hong <hongliuforever@gmail.com>
2019-06-26 20:22:46 -0500

    Add files via upload

---
+++ 8_change.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+int get_change(int m) {
+	int tenth = m / 10;
+	int fifth = (m % 10) / 5;
+	int oneth = m % 5;
+	return (tenth + fifth + oneth);
+}
+
+int main() {
+	int m;
+	std::cin >> m;
+	std::cout << get_change(m) << '\n';
+}
+
+
